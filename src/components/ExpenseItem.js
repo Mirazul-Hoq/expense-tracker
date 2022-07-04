c1d81f9c6c044ed96366c05ad43f5f8639dcbf3b
Mirazul-Hoq <mirazulhoq807@gmail.com>
2022-07-04 07:38:09 +0600

    expense item dynamic setup completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
-        <div>March 10th, 2022</div>
+    <div className='expense-item' key={ props.expense.id }>
+        <div>{ props.expense.date.toISOString() }</div>
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$249.44</div>
+            <h2>{ props.expense.title }</h2>
+            <div className='expense-item__price'>${ props.expense.amount }</div>
         </div>
     </div>
   )
