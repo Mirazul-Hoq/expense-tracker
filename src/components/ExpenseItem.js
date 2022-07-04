96f24fffd869c5703d6abb1bd2db2b61fae8211b
Mirazul-Hoq <mirazulhoq807@gmail.com>
2022-07-04 07:44:58 +0600

    expense item date design changed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    const month = props.expense.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.expense.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.expense.date.getFullYear();
+
   return (
     <div className='expense-item' key={ props.expense.id }>
-        <div>{ props.expense.date.toISOString() }</div>
+        <div>
+            <div>{ month }</div>
+            <div>{ day }</div>
+            <div>{ year }</div>
+        </div>
         <div className='expense-item__description'>
             <h2>{ props.expense.title }</h2>
             <div className='expense-item__price'>${ props.expense.amount }</div>
